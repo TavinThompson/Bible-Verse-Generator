1daec7b600d7276bcb2122c4ecf6457e6c9be730
TavinThompson <toasted.tazzie@gmail.com>
2026-08-11 16:19:27 -0400

    Add generic quotes to quotes.js

---
+++ quotes.js
@@ -0,0 +1,33 @@
+const quotes = [
+
+  "Start before you feel ready.",
+
+  "Small steps become big changes.",
+
+  "You don't need more time. You need to use the time you have.",
+
+  "Your future is built by what you do today.",
+
+  "Keep going. You're closer than you think.",
+
+  "A difficult road can still lead somewhere beautiful.",
+
+  "Do something today that your future self will appreciate.",
+
+  "Consistency will take you places motivation never could.",
+
+  "You can start over without starting from nothing.",
+
+  "One good decision can change the direction of your entire day.",
+
+  "Be willing to be a beginner.",
+
+  "Progress doesn't have to be dramatic to be real.",
+
+  "The life you want is built one ordinary day at a time.",
+
+  "Take the risk or lose the chance.",
+
+  "Make today worth remembering."
+
+];
